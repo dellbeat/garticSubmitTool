9ba7dbd65a4753d28c089f1d2e937bb4c1de3088
dellbeat <daizhicai@foxmail.com>
2021-03-12 23:07:12 +0800

    添加项目文件。

---
+++ garticSubmitTool/RequestEntity.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace garticSubmitTool
+{
+    public class RequestEntity
+    {
+        /// <summary>
+        /// 语言代号，默认16
+        /// </summary>
+        public int language { get; set; }
+        /// <summary>
+        /// 种类代号，默认30，代表其他
+        /// </summary>
+        public int subject { get; set; }
+        /// <summary>
+        /// 添加的内容，对象为字符串列表，列表内元素分别为词汇和难度代号（默认0，即简单），如["1","0"]
+        /// </summary>
+        public List<List<string>> added { get; set; }
+        /// <summary>
+        /// 删除的内容，格式可能也同上
+        /// </summary>
+        public List<List<string>> removed { get; set; }
+    }
+}
